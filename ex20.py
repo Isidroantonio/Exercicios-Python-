94d9d1676b98f24fc26bf5d6e400fa9706593773
Isidro D'antonio <dev.isidrojunior@gmail.com>
2022-07-23 10:19:35 -0300

    Atualizações

--- ex20.py
+++ ex20.py
@@ -1,13 +1,19 @@
 # Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o
-#  fatorial várias vezes e limitando o fatorial a números inteiros positivos
-#  e menores que 16.
+# fatorial várias vezes e limitando o fatorial a números inteiros positivos
+# e menores que 16.
 
 from math import factorial
+
+def fatorial():
+    numInt = int(input('qual número deseja fatorar ?'))
+
+    if numInt > 16 or numInt < 0:
+        print('Fatorial não permitida, entre com um número entre 1 e 16: ')
+        fatorial()
+    else:
+        print(factorial(numInt))
 
 print('-'*30)
 print('Fatoração')
 print('-'*30)
-
-numInt = int(input('qual número deseja fatorar ?'))
-
-print(factorial(numInt))
+fatorial()
