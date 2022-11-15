dc7725a1ee1d9592e2bfc5f90859c40cf0e8090c
Isidro Junior <dev.isidrojunior@gmail.com>
2022-11-15 17:19:05 -0300

    adcionado função

--- ex12.py
+++ ex12.py
@@ -1,15 +1,19 @@
 # Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro 
 # entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a tabuada. 
 
+import math
 
-print('-'*30)
-print('Gerador de Tabuadas.')
-print('-'*30)
+def tabuadas():
 
-tabuadaValor = int(input('Qual tabuada deseja calcular: '))
-valorMax = int(input('Qual valor maxímo deseja calcular da tabuada {}: '.format(tabuadaValor)))
+    print('-'*30)
+    print('Gerador de Tabuadas.')
+    print('-'*30)
 
-for i in range(1, valorMax+1):
-    print(tabuadaValor, 'x', i, '=', tabuadaValor*i)
+    tabuadaValor = int(input('Qual tabuada deseja calcular: '))
+    valorMax = int(input('Qual valor maxímo deseja calcular da tabuada {}: '.format(tabuadaValor)))
+    for i in range(1, valorMax+1):
+        print(tabuadaValor, 'x', i, '=', tabuadaValor*i)
     
+tabuadas()
+        
         
